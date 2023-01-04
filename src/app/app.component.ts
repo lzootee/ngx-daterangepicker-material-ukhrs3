c433184687487d5fc9fbfd22f0c75aa508e193a1
lzootee <lzootee@gmail.com>
2023-01-04 23:11:43 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular 6';
   selected;
+  ranges: any = {
+    'Today': [moment(), moment()],
+    'Yesterday': [moment(), moment()],
+    'Last 7 Days': [moment(), moment()],
+    'Last 30 Days': [moment(), moment()],
+    'This Month': [moment(), moment()],
+    'Last Month':[moment(), moment()],
+  }
   getMinDate(): any { return moment().subtract(1, 'years').add(1, 'days'); } 
   getMaxDate(): any { return moment(); }
 }
